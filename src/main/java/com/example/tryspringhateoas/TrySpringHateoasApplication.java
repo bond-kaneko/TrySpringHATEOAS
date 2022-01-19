92d8c5e96172139d44b1cdcb306901153e6ce071
junki.kaneko <junki.kaneko@linecorp.com>
2022-01-19 17:55:33 +0900

    hateoas

---
+++ src/main/java/com/example/tryspringhateoas/TrySpringHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryspringhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringHateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrySpringHateoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tryspringhateoas/TrySpringHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryspringhateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySpringHateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
